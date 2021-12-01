13630ec8c6587c87858526084e614039142d4668
Christophe Bliard <christophe.bliard@trux.info>
2021-12-01 15:14:50 +0100

    day1: part 2 implemented

--- day1/challenge1.rb
+++ day1/challenge1.rb
@@ -21,6 +21,24 @@ RSpec.describe "count_sonar_depth_increases" do
   end
 end
 
+RSpec.describe "count_sonar_depth_sliding_increases" do
+  it "returns the number of times the depth increases with a sampling window of 3 measures" do
+    input = <<~DEPTHS
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
+    DEPTHS
+    expect(count_sonar_depth_sliding_increases(StringIO.new(input))).to eq(5)
+  end
+end
+
 
 def count_sonar_depth_increases(io = nil)
   if io.nil?
@@ -34,10 +52,24 @@ def count_sonar_depth_increases(io = nil)
     .count("increased")
 end
 
+def count_sonar_depth_sliding_increases(io = nil)
+  if io.nil?
+    return open(File.join(__dir__, "challenge1-input.txt")) { |f| count_sonar_depth_sliding_increases(f) }
+  end
+
+  io.readlines
+    .map(&:to_i)
+    .each_cons(3)
+    .map(&:sum)
+    .each_cons(2)
+    .map { |a, b| { -1 => "increased", 0 => "same", 1 => "decreased" }[a <=> b] }
+    .count("increased")
+end
 
 if $0 == __FILE__
   if ARGV[0] == "run"
-    puts count_sonar_depth_increases
+    puts "part 1: #{count_sonar_depth_increases}"
+    puts "part 2: #{count_sonar_depth_sliding_increases}"
   else
     RSpec::Core::Runner.run([])
   end
